5aee6095e9695a3afb6e823b4dfab3ff431d55da
Eldor99 <eldor1999@protonmail.com>
2020-12-29 16:52:30 +0500

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
+
+const Hats = () => {
+  return <div>Hays</div>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
